91b1d9d22476b186ab0dad9ff59881377044fb85
Guido Tarantelli <ciarachella@gmail.com>
2025-06-30 11:30:45 +0200

    use dynamic import for prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import type { PrismaClient } from '@prisma/client'
+
+// use require so build succeeds even if the Prisma client isn't generated yet
+const PrismaClientConstructor =
+  (require('@prisma/client') as typeof import('@prisma/client')).PrismaClient
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient()
+  new PrismaClientConstructor()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
