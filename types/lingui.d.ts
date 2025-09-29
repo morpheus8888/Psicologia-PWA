fd9f68d92591d8165ae7de1a6a62940e94989e3a
Guido <ciarachella@gmail.com>
2025-09-29 09:10:29 +0200

    Fix Lingui message-utils typings

--- types/lingui.d.ts
+++ types/lingui.d.ts
@@ -1,15 +1,32 @@
 declare module '@/locales/*/messages' {
-  export const messages: import('@lingui/core').MessageCatalog
-  const catalog: {
-    messages: import('@lingui/core').MessageCatalog
-  }
-  export default catalog
-}
-
-declare module '@lingui/message-utils' {
-  export * from '@lingui/message-utils/dist/compileMessage'
+	export const messages: import('@lingui/core').MessageCatalog
+	const catalog: {
+		messages: import('@lingui/core').MessageCatalog
+	}
+	export default catalog
 }
 
 declare module '@lingui/message-utils/compileMessage' {
-  export * from '@lingui/message-utils'
+	export type CompiledIcuChoices = Record<string, CompiledMessage> & {
+		offset: number | undefined
+	}
+
+	export type CompiledMessageToken =
+		| string
+		| [name: string, type?: string, format?: null | string | unknown | CompiledIcuChoices]
+
+	export type CompiledMessage = CompiledMessageToken[]
+
+	export type MapTextFn = (value: string) => string
+
+	export function compileMessage(message: string, mapText?: MapTextFn): CompiledMessage
+
+	export function compileMessageOrThrow(
+	message: string,
+	mapText?: MapTextFn
+): CompiledMessage
+}
+
+declare module '@lingui/message-utils' {
+	export * from '@lingui/message-utils/compileMessage'
 }
